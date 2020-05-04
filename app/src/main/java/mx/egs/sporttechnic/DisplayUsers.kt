dff0e5c3187c538c5bbd0ec0d097765df2c040b9
egall <a01745776@itesm.mx>
2020-05-03 20:05:45 -0500

    pantalla usuario

--- app/src/main/java/mx/egs/sporttechnic/DisplayUsers.kt
+++ app/src/main/java/mx/egs/sporttechnic/DisplayUsers.kt
@@ -3,7 +3,9 @@ package mx.egs.sporttechnic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.text.set
 import com.google.firebase.auth.FirebaseAuth
@@ -16,6 +18,7 @@ import kotlinx.android.synthetic.main.activity_display_users.*
 import javax.xml.validation.Validator
 
 class DisplayUsers : AppCompatActivity() {
+
     private lateinit var baseDatos: FirebaseDatabase
     private lateinit var auth: FirebaseAuth
 
@@ -25,12 +28,51 @@ class DisplayUsers : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         baseDatos = FirebaseDatabase.getInstance()
+        displayData()
     }
 
-    fun displayData(){
+    private fun displayData(){
+        val userId = auth.currentUser?.uid
+        var DBReference = baseDatos.getReference("/Users/$userId")
+        DBReference.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(this@DisplayUsers, "Falla al descargar los entrenamientos", Toast.LENGTH_SHORT).show()
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (registro in snapshot.children){
+                    when (registro.key){
+                        "name" -> tvName.text = registro.value as CharSequence?
+                        "gender" -> tvGender.text = registro.value as CharSequence?
+                        "mail" -> tvMail.text = registro.value as CharSequence?
+                        else -> tvBirthdate.text = registro.value as CharSequence?
+                    }
+                    /*tvName.text = registro.key()
+                    tvGender.text = user?.gender
+                    tvMail.text = user?.mail
+                    tvBirthdate.text = user?.birthday
+                    if(registro.key == userId){
+                        Log.i("BD chida", "$registro")
+                        val user = registro.getValue(Usuario::class.java)
+                        tvName.text = user?.name
+                        tvGender.text = user?.gender
+                        tvMail.text = user?.mail
+                        tvBirthdate.text = user?.birthday
+                    }*/
+                    Log.i("BD", "$registro")
+                }
+            }
+
+        })
 
     }
 
+    fun changePasword(view: View){
+        val mail = auth.currentUser?.email
+        if (mail != null) {
+            auth.sendPasswordResetEmail(mail)
+            Toast.makeText(this, "Se ha enviado un correo a $mail para restablecer la contraseña", Toast.LENGTH_LONG).show()
+        }
+    }
 
     fun cerrarSesion(view: View){
         auth.signOut()
